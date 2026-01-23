211fddda283045ce07e2e6f4450a4d87773f62a0
Marshall Walls <marshallwalls@gmail.com>
2026-01-22 22:27:14 -0500

    Add items table and user-item relationship

---
+++ app/Models/Item.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Item extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'quantity',
+        'location',
+        'expiration_date',
+        'notes',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
